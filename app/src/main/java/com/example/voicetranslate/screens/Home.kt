869529f4ab516456dc9401f519a7ac19ae537d2a
LeCongTung <tung.100301@gmail.com>
2022-05-09 23:34:39 +0700

    Code UI

---
+++ app/src/main/java/com/example/voicetranslate/screens/Home.kt
@@ -0,0 +1,12 @@
+package com.example.voicetranslate.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.voicetranslate.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/voicetranslate/screens/activities/Show_Content.kt
@@ -0,0 +1,12 @@
+package com.example.voicetranslate.screens.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.voicetranslate.R
+
+class Show_Content : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_content)
+    }
+}
